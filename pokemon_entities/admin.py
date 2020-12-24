b9dbc5a9785d1e0eb89320997643bcf5144a6312
Aleksey Koshkin <ekbdizzy@yandex.ru>
2020-12-24 18:16:48 +0300

    add PokemonEntity model

--- pokemon_entities/admin.py
+++ pokemon_entities/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
-from .models import Pokemon
+from .models import Pokemon, PokemonEntity
 
 
 @admin.register(Pokemon)
 class PokemonAdmin(admin.ModelAdmin):
     pass
+
+
+admin.site.register(PokemonEntity)
--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,10 +1,19 @@
 from django.db import models
+import math
 
 
-# your models here
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="pokemons", null=True, blank=True)
 
     def __str__(self):
         return self.title
 
+
+class PokemonEntity(models.Model):
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    lat = models.FloatField()
+    lon = models.FloatField()
+
+    def __str__(self):
+        return f"{round(lat, 3)}: {round(lon, 3)}"
