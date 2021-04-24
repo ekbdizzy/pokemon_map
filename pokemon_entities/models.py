4c6b9e93b6eae992ea61a39b971b1fa00b0489b3
Aleksey Koshkin <ekbdizzy@yandex.ru>
2021-04-24 18:23:41 +0300

    add description field to Pokemon model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -5,6 +5,7 @@ import math
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(upload_to="pokemons", null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.title
